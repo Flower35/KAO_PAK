d4d7d12a0a8483080d0fec4abcf13ead90e2d079
Radek Krzyśków <46760021+Flower35@users.noreply.github.com>
2019-11-17 13:49:30 +0100

    Add files via upload

---
+++ include/KAO_PAK/PakExporter.h
@@ -0,0 +1,50 @@
+#ifndef H_PAK_EXPORTER
+#define H_PAK_EXPORTER
+
+#include <KAO_PAK/ConsoleApp.h>
+
+
+////////////////////////////////////////////////////////////////
+// PAK EXPORTER CLASS
+////////////////////////////////////////////////////////////////
+
+class PakExporter
+{
+    public:
+
+    /*** Properties ***/
+
+        std::string OutputDir;
+
+        std::string PakName;
+
+        int32_t FilesCount;
+        int32_t FileTableOffset;
+
+        std::ifstream PakFile;
+        std::ofstream ItemFile;
+        uint8_t* Data;
+
+        std::ofstream LogFile;
+        bool SaveLog;
+
+    /*** Methods ***/
+
+        bool saveItem(int32_t filesize, char* filename);
+
+        bool exportArchive();
+
+        bool checkStreamSize(int32_t size);
+
+        bool openAndCheckArchive();
+
+        bool checkPakFileExtension();
+
+        void getPakFilenameFromPath();
+
+        PakExporter(char* pak, char* directory, bool log);
+
+        ~PakExporter();
+};
+
+#endif
---
+++ source/KAO_PAK/ConsoleApp.cpp
@@ -0,0 +1,131 @@
+#include <KAO_PAK/PakExporter.h>
+#include <KAO_PAK/PakImporter.h>
+
+int main(int argc, char** argv)
+{
+    std::string common_path;
+    std::string answer;
+    bool test = false;
+    bool no_errors = true;
+
+    /* Welcome! */
+
+    std::cout << "\n < KAO_PAK >"
+        << "\n Simple utility to extract files from"
+        << "\n \"Kao the Kangaroo\" PAK archives."
+        << "\n";
+
+    if (argc < 2)
+    {
+        std::cout << "\n Please close this window and drop"
+            << "\n one or more of KAO PAK archives"
+            << "\n onto this executable!"
+            << "\n (press any key to close)"
+            << "\n";
+
+        std::cin.ignore();
+        return (-1);
+    }
+
+    /* Get ready */
+
+    std::cout << "\n Choose an option:"
+        << "\n 1) passed PAK archives as arguments [UNPACK FILES]"
+        << "\n 2) passed LOG lists of files as arguments [REPACK ARCHIVES]"
+        << "\n >> ";
+
+    std::getline(std::cin, common_path);
+
+    switch (std::stoi(common_path))
+    {
+        case 1:
+        {
+            std::cout << "\n Please write a common path for all files"
+                << "\n or leave empty for separate directories."
+                << "\n >> ";
+
+            std::getline(std::cin, common_path);
+
+            if (common_path.length() > 0)
+            {
+                if ((common_path.back() != '/') && (common_path.back() != '\\'))
+                {
+                    common_path += '\\';
+                }
+            }
+
+            /* Ask another question */
+
+            std::cout << "\n Do you want to create LOG files"
+                << "\n for repacking? [Y - yes]"
+                << "\n >> ";
+
+            std::getline(std::cin, answer);
+
+            if (answer.length() > 0)
+            {
+                if ('y' == tolower(answer.at(0)))
+                {
+                    test = true;
+                }
+            }
+
+            /* Iterate through every argument */
+
+            for (int i = 1; (i < argc) && no_errors; i++)
+            {
+                std::cout << "\n --------------------------------"
+                    << "\n \"" << argv[i] << "\""
+                    << "\n";
+
+                PakExporter pak(argv[i], (char*)common_path.c_str(), test);
+
+                if (pak.openAndCheckArchive())
+                {
+                    no_errors = pak.exportArchive();
+                }
+                else
+                {
+                    no_errors = false;
+                }
+            }
+
+            break;
+        }
+
+        case 2:
+        {
+
+            /* Iterate through every argument */
+
+            for (int i = 1; (i < argc) && no_errors; i++)
+            {
+                std::cout << "\n --------------------------------"
+                    << "\n \"" << argv[i] << "\""
+                    << "\n";
+
+                PakImporter pak(argv[i]);
+
+                if (pak.createArchive())
+                {
+                    no_errors = pak.importData();
+                }
+                else
+                {
+                    no_errors = false;
+                }
+            }
+
+            break;
+        }
+    }
+
+    /* The end :) */
+
+    std::cout << "\n --------------------------------"
+        << "\n (press any key to close)"
+        << "\n";
+
+    std::cin.ignore();
+    return 0;
+}
